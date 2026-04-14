8a3439fff84952e4dea2334c46c0f65d105248d3
C.Y. Chan <2460157@dundee.ac.uk>
2026-04-14 16:57:09 +0000

    variable test

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask , request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'tutorhand'
+
+@app.route('/tutor/<id>')
+def tutor(id):
+    return f'your id is {id}'
+
+@app.route('/search')
+def search():
+    topicSearch = request.args.get('topic','nothing')
+    return f'you searched for {topicSearch}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
